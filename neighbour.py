1b707cf1f704a83399a8feada85e89a82f631d07
berg <bergtian@qq.com>
2016-10-28 10:32:40 +0800

    hilight output

--- neighbour.py
+++ neighbour.py
@@ -3,6 +3,25 @@
 from __future__ import print_function
 import sys
 
+
+class colors:
+    ''' Class used to set console colors '''
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+
+    @classmethod
+    def disable(cls):
+        cls.HEADER = ''
+        cls.OKBLUE = ''
+        cls.OKGREEN = ''
+        cls.WARNING = ''
+        cls.FAIL = ''
+        cls.ENDC = ''
+
 try:
 	lineno = int(sys.argv[1])
 	file_path = sys.argv[2]
@@ -18,6 +37,14 @@ except:
 line_min = min(abs(lineno - 3),lineno)
 line_max = lineno+7
 
+
+
 with open(file_path) as f:
 	for n,l in enumerate(f):
-		if n >= line_min and n <= line_max:print("{}:{}".format(n,l),end="")
+		if n >= line_min and n <= line_max:
+			header_color = ('',colors.HEADER)[lineno == n]
+			print("{}{:<4}: {}{}".format(header_color,n,l,colors.ENDC),end="")
+
+print("")
+
+#a=linecache.getlines('a.txt')[0:4]
